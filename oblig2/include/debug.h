1357c28b8af7f085d1e3c8bd1f4b4e6c8b5b4de5
Bendik Samseth <b.samseth@gmail.com>
2016-04-09 18:29:48 +0200

    Added functions to be used in debugging

---
+++ oblig2/include/debug.h
@@ -0,0 +1,8 @@
+#ifndef DEBUG_FUNCTIONS_H
+#define DEBUG_FUNCTIONS_H
+
+void print_matrix(double** A, int m, int n);
+void rand_mat(double** A, int m, int n, int MAX_VALUE);
+void std_mat(double** A, int m, int n);
+
+#endif
---
+++ oblig2/src/debug.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+#include "debug.h"
+
+void print_matrix(double** A, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%3f ", A[i][j]);
+        printf("\n");
+    }
+}
+
+void rand_mat(double** A, int m, int n, int MAX_VALUE) {
+    /* Generating Random Values for A array*/
+    srand(time(NULL));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            A[i][j] = rand() % MAX_VALUE;
+    }
+}
+
+void std_mat(double** A, int m, int n) {
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            A[i][j] = ++count;
+    }
+}

e6b4b2ac3e18c5f756ff710e11b312db8dabaa73
Bendik Samseth <b.samseth@gmail.com>
2016-04-11 14:29:48 +0200

    Upgrade matrix multiplication funciton
    The used function now uses both OpenMP to parallelize the triple loop,
    but also uses the transpose of B. This is done so that the lookup of
    the values are cache-friendly, see
    [this](http://stackoverflow.com/questions/22634121/openmp-c-matrix-multi
    plication-run-slower-in-parallel).
    
    The latter gives a small boost, it takes some time to transpose B, but
    it’s still faster to to so.

--- oblig2/include/matrixmultiply.h
+++ oblig2/include/matrixmultiply.h
@@ -2,5 +2,6 @@
 #define MATRIX_MULTIPLY_H
 
 void multiply(double* A, double* B, double* C, int m, int l, int n);
+void transpose(double *A, double *B, int m, int n);
 
 #endif
--- oblig2/src/matrixmultiply.c
+++ oblig2/src/matrixmultiply.c
@@ -1,13 +1,54 @@
 #include "matrixmultiply.h"
 #include "binaryformat.h"
+#include "omp.h"
 
+/*
+ * Compute C = A * B.
+ * Uses OpenMP to optimize the tripple loop.
 
 void multiply(double* A, double* B, double* C, int m, int l, int n) {
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < l; k++) {
-                C[i*m+j] += A[i*m+k]*B[k*l+j];
+    #pragma omp parallel
+    {
+        int i, j, k;
+        #pragma omp for
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                double dot = 0;
+                for (k = 0; k < l; k++) {
+                    dot += A[i * l + k] * B[k * n + j];
+                }
+                C[i * n + j] += dot;
             }
+        }
+    }
+}
+*/
+
+void multiply(double* A, double* B, double* C, int m, int l, int n) {
+    double *B2;
+    B2 = malloc(l*n * sizeof *B2);
+    transpose(B, B2, l, m);
+    #pragma omp parallel
+    {
+        int i, j, k;
+        #pragma omp for
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                double dot = 0;
+                for (k = 0; k < l; k++) {
+                    dot += A[i * l + k] * B2[j * l + k];
+                }
+                C[i * n + j] += dot;
+            }
+        }
+    }
+    free(B2);
+}
+
+void transpose(double *A, double *B, int m, int n) {
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            B[j * m + i] = A[i * n + j];
         }
     }
 }

e3036aaac7780900f20b9f87ad4bda54b29bbcf0
Bendik Samseth <b.samseth@gmail.com>
2016-05-15 14:02:46 +0200

    Fix bug in matrixmultiply
    Had wrong dimensions on the transposed matrix. Working now.

--- oblig2/src/matrixmultiply.c
+++ oblig2/src/matrixmultiply.c
@@ -2,32 +2,11 @@
 #include "binaryformat.h"
 #include "omp.h"
 
-/*
- * Compute C = A * B.
- * Uses OpenMP to optimize the tripple loop.
-
-void multiply(double* A, double* B, double* C, int m, int l, int n) {
-    #pragma omp parallel
-    {
-        int i, j, k;
-        #pragma omp for
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < n; j++) {
-                double dot = 0;
-                for (k = 0; k < l; k++) {
-                    dot += A[i * l + k] * B[k * n + j];
-                }
-                C[i * n + j] += dot;
-            }
-        }
-    }
-}
-*/
 
 void multiply(double* A, double* B, double* C, int m, int l, int n) {
     double *B2;
     B2 = malloc(l*n * sizeof *B2);
-    transpose(B, B2, l, m);
+    transpose(B, B2, l, n);
     #pragma omp parallel
     {
         int i, j, k;
